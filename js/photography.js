485b7731b09d630c3a7cb0928fbfefbb87aa8213
Liam Kreiss <liam.kreiss@gmail.com>
2020-03-30 16:33:23 -0700

    added all_photos page

--- js/photography.js
+++ js/photography.js
@@ -3,6 +3,21 @@ all_photos["Across America"] = "000015430004.jpg";
 all_photos["New England"] = "000471250018.jpg";
 all_photos["Form and Function"] = "000017440027.jpg";
 
+var photos_1 = {};
+photos_1["Across America"] = "000015430004.jpg";
+photos_1["blonde girl"] = "000015440001.jpg";
+photos_1["ping pong ball"] = "000020770019.jpg";
+
+var photos_2 = {};
+photos_2["New England"] = "000471250018.jpg";
+photos_2["volleyball"] = "000023400029.jpg";
+photos_2["door"] = "000359880023-2.jpg";
+
+var photos_3 = {};
+photos_3["Form and Function"] = "000017440027.jpg";
+photos_3["eye ball"] = "000020770023-4.png";
+photos_3["skyline"] = "74980019-Corrected.png";
+
 function show_title(photo_id) {
   var cur_photo_element = document.getElementById(photo_id);
   var cur_hidden_elements = cur_photo_element.querySelectorAll(".hidden_element");
